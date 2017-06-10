6a944993d56e4d1b2655cbda9a0948d47c523be4
Lucas Alves <lucashalves@live.com>
2017-06-10 20:03:15 -0300

    Continuing service setPrediction (in progress)

---
+++ src/repositories/guessLines/setPredictionsRepository.js
@@ -0,0 +1,21 @@
+'use Strict';
+
+module.exports = (app) => {
+  const GuessLines = app.coincidents.Schemas.guessesLinesSchema;
+
+  const setPredictions = (request, dictionary) => {
+
+    const searchQuery = {
+      championship: request.championshipId
+    }
+
+    return GuessLines
+      .find(searchQuery)
+      .then(() => )
+      .catch((err) => err)
+  }
+
+  return {
+    setPredictions
+  }
+}
