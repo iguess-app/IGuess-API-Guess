0802ed6eb6cdfa2756a6772fc3efa0e6fb954e26
Lucas Alves <lucashalves@live.com>
2017-08-29 00:11:57 -0300

    Adding plugins on appJs instead configServer (Cuz async problems)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const consign = require('consign')
 const app = {}
 app.coincidents = require('iguess-api-coincidents')
+const plugins = require('./src/plugins/serverPlugins')(app.coincidents.Config)
 
 consign()
   .include('configServer.js')
@@ -13,11 +14,14 @@ consign()
   .include('src/routes')
   .into(app);
 
-app.configServer.start((err) => {
-  if (err) {
-    throw err;
-  }
-  app.configServer.logger().info(`Server running at ${app.configServer.info.uri}`)
+
+app.configServer.register(plugins, () => {
+  app.configServer.start((err) => {
+    if (err) {
+      throw err;
+    }
+    app.configServer.logger().info(`Server running at ${app.configServer.info.uri}`)
+  })
 })
 
 module.exports = app
--- configServer.js
+++ configServer.js
@@ -2,18 +2,14 @@
 
 const Hapi = require('hapi');
 
-const plugins = require('./src/plugins/serverPlugins')
-
 module.exports = (app) => {
-  const config = app.coincidents.Config;
+  const config = app.coincidents.Config
 
   const server = new Hapi.Server();
   server.connection({
     host: '0.0.0.0',
     port: config.serverPort
   });
-
-  server.register(plugins);
 
   return server;
 }
