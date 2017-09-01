3f61c56ab7cabd215e4e96681f2927de1cb84f65
Lucas Alves <lucashalves@live.com>
2017-08-31 23:41:19 -0300

    Added Cron, Add service to addNewGuessLines (Wip)

---
+++ src/cron/addGuessLinesRoutine.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const CronJob = require('cron').CronJob
+
+const Seconds = 20
+const Minutes = 39
+const Hours = 23
+const fullHour = `${Seconds} ${Minutes} ${Hours}`
+const dayOfMonth = '*'
+const months = '*'
+const dayOfWeek = '*'
+
+module.exports = (app) => {
+  const requestManager = app.coincidents.Managers.requestManager
+
+  return new CronJob(`${fullHour} ${dayOfMonth} ${months} ${dayOfWeek}`, () => {
+
+    const url = 'http://localhost:9001/championship/getAllchampionship'
+    const headers = {
+      'language': 'en-us',
+      'content-type': 'application/json'
+    }
+    requestManager.get(url, headers)
+      .then((championships) => {
+        _addGuessLines(app, championships)
+      })
+
+  }, null, true, 'America/Sao_Paulo')
+}
+
+
+const _addGuessLines = (app, championships) => {
+  const GuessLines = app.coincidents.Schemas.guessesLinesSchema;
+  championships.map((championship) => {
+    const searchQuery = {
+      'championship.championshipRef': championship._id
+    }
+    GuessLines.findOne(searchQuery)
+    .then((guessLine) => {
+      if (!guessLine) {
+        _insertNewGuessLine(championship)
+      }
+    })
+  })
+}
+
+const _insertNewGuessLine = (championship) => {
+  
+}
