6d1da2453e97db779308b8256531b1d0fb165577
Lucas Alves <lucashalves@live.com>
2017-09-02 14:13:14 -0300

    Update addGuessLineRoutine

--- src/cron/addGuessLinesRoutine.js
+++ src/cron/addGuessLinesRoutine.js
@@ -2,50 +2,68 @@
 
 const CronJob = require('cron').CronJob
 
-const Seconds = 20
-const Minutes = 39
-const Hours = 23
+const Seconds = 30 //0
+const Minutes = 12 //0
+const Hours = 14 //0
 const fullHour = `${Seconds} ${Minutes} ${Hours}`
 const dayOfMonth = '*'
 const months = '*'
 const dayOfWeek = '*'
 
 module.exports = (app) => {
+  const GuessLines = app.coincidents.Schemas.guessesLinesSchema
   const requestManager = app.coincidents.Managers.requestManager
 
-  return new CronJob(`${fullHour} ${dayOfMonth} ${months} ${dayOfWeek}`, () => {
-    //TODO usar app.coincidents.apis.holiUrl na url
-    const url = 'http://localhost:9001/championship/getAllchampionship'
+  const cronJob = () => new CronJob(`${fullHour} ${dayOfMonth} ${months} ${dayOfWeek}`, () => {
+
+    const url = `${app.coincidents.Config.apis.holiUrl}/championship/getAllchampionship`
     const headers = {
       'language': 'en-us',
       'content-type': 'application/json'
     }
     requestManager.get(url, headers)
-      .then((championships) => {
-        _addGuessLines(app, championships)
-      })
+      .then((championships) => _addGuessLines(championships))
 
   }, null, true, 'America/Sao_Paulo')
-}
 
 
-const _addGuessLines = (app, championships) => {
-  const GuessLines = app.coincidents.Schemas.guessesLinesSchema;
-  championships.map((championship) => {
-    const searchQuery = {
-      'championship.championshipRef': championship._id
-    }
-    GuessLines.findOne(searchQuery)
-    .then((guessLine) => {
-      if (!guessLine) {
-        _insertNewGuessLine(championship)
-        return
+  const _addGuessLines = (championships) => {
+    championships.map((championship) => {
+      const searchQuery = {
+        'championship.championshipRef': championship._id
       }
-      //TODO update nas flags isActive da GuessLine que já existir
-    })
-  })
-}
 
-const _insertNewGuessLine = (championship) => {
-   
+      return GuessLines.findOne(searchQuery)
+        .then((guessLine) => {
+          if (!guessLine) {
+            return _insertNewGuessLine(championship)
+          }
+
+          return _updateFlagIsActive(championship, guessLine)
+        })
+    })
+  }
+
+  const _insertNewGuessLine = (championship) => {
+    const newGuessLineObj = {
+      championship: {
+        championshipRef: championship._id,
+        league: championship.league,
+        season: championship.season,
+        championship: championship.championship
+      },
+      guessLineActive: championship.championshipActive,
+      usersAddedAtGuessLine: []
+    }
+
+    return GuessLines.create(newGuessLineObj)
+  }
+
+  const _updateFlagIsActive = (championship, guessLine) => {
+    guessLine.guessLineActive = championship.championshipActive
+
+    return guessLine.save()
+  }
+
+  cronJob()
 }
