ee0880f963306b7f09f87c05de75774bd0e512bc
Lucas Alves <lucashalves@live.com>
2017-09-21 01:03:42 -0300

    Verifying if all the users added to guessLeague is on guessLine championship

---
+++ src/repositories/guessLines/verifyUserAtGuessLineRepository.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Boom = require('boom')
+
+module.exports = (app) => {
+  const GuessLine = app.src.models.guessesLinesModel
+  const StatusUtils = app.coincidents.Utils.statusUtils
+
+  const verifyUserAtGuessLine = (request, dictionary) => {
+    const searchQuery = _buildSearchQuery(request)
+
+    return GuessLine.findOne(searchQuery)
+      .then((guesslineFound) => {
+        _checkErrors(guesslineFound, dictionary, request, StatusUtils)
+
+        return {
+          userRefAtGuessLineList: true
+        }
+      })
+  }
+
+  return verifyUserAtGuessLine
+}
+
+const _buildSearchQuery = (request) => {
+  const searchQuery = {
+    'championship.championshipRef': request.championshipRef,
+    usersAddedAtGuessLine: {
+      $in: [request.userRef]
+    }
+  }
+
+  return searchQuery
+}
+
+const _checkErrors = (guesslineFound, dictionary, request, StatusUtils) => {
+  if (!guesslineFound) {
+    throw Boom.create(StatusUtils.notFound, dictionary.notAtGuessLine, {request})
+  }
+}
+
+/*eslint max-params: [2, 4]*/
--- src/services/guessLeagues/createGuessLeagueService.js
+++ src/services/guessLeagues/createGuessLeagueService.js
@@ -1,17 +1,38 @@
 'use strict'
 
+const Promise = require('bluebird')
+
 module.exports = (app) => {
   const createGuessLeagueRepository = app.src.repositories.guessLeagues.createGuessLeagueRepository
   const getChampionshipAtGuessLineRepository = app.src.repositories.guessLines.getChampionshipAtGuessLineRepository
+  const verifyUserAtGuessLineRepository = app.src.repositories.guessLines.verifyUserAtGuessLineRepository
 
   const createGuessLeague = (payload, headers) => {
     const dictionary = app.coincidents.Translate.gate.selectLanguage(headers.language)
 
-    return getChampionshipAtGuessLineRepository.getChampionshipAtGuessLine(payload, dictionary)
-      .then((championship) => createGuessLeagueRepository.createGuessLeague(payload, championship, dictionary))
+    return _verifyIfUsersSentIsOnGuessLineList(verifyUserAtGuessLineRepository, payload, dictionary)
+      .then(() => getChampionshipAtGuessLineRepository(payload, dictionary))
+      .then((championship) => createGuessLeagueRepository(payload, championship, dictionary))
   }
 
   return {
     createGuessLeague
   }
+}
+
+const _verifyIfUsersSentIsOnGuessLineList = (verifyUserAtGuessLineRepository, payload, dictionary) => {
+  if (payload.inviteads) {
+    const inviteadsCheckArrayPromise = payload.inviteads.map((userRefInvited) => {
+      const verifyUserAtGuessLineObj = {
+        championshipRef: payload.championshipRef,
+        userRef: userRefInvited
+      }
+
+      return verifyUserAtGuessLineRepository(verifyUserAtGuessLineObj, dictionary)
+    })
+
+    return Promise.map(inviteadsCheckArrayPromise, (justReturn) => justReturn)
+  }
+
+  return payload
 }
