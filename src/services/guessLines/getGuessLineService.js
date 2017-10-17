507312361e3219b2c3eb803ed00240b2f0a1bc35
Lucas Alves <lucashalves@live.com>
2017-10-16 22:34:52 -0200

    getGuessLine refactoring, isolating some functions and added some TODOs

--- src/services/guessLines/getGuessLineService.js
+++ src/services/guessLines/getGuessLineService.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const coincidents = require('iguess-api-coincidents')
-const selectLanguage = coincidents.Translate.gate.selectLanguage
-
 const moment = require('moment')
 const Promise = require('bluebird')
+
+const selectLanguage = coincidents.Translate.gate.selectLanguage
 
 const getPontuationsRepository = require('../../repositories/guessLines/getPontuationsRepository')
 const getPredictionsRepository = require('../../repositories/guessLines/getPredictionsRepository')
@@ -15,70 +15,95 @@ const getGuessLine = (request, headers) => {
   const dictionary = selectLanguage(headers.language)
 
   return getGuessLineRepository(request, dictionary)
-    .then((guessLine) => {
-      const getLastRoundObj = {
-        championshipRef: guessLine.championship.championshipRef,
-        userRef: request.userRef
-      }
-      return Promise.all([
-        getPontuationsRepository(getLastRoundObj),
-        getLastRoundRepository(getLastRoundObj), 
-        guessLine])
-    })
-    .then((guessLineAndlastRound) => {
-      const userPontuation = guessLineAndlastRound[0]
-      const lastRound = guessLineAndlastRound[1]
-      const guessLine = guessLineAndlastRound[2]
-      const matchDayDate = moment(lastRound.unixDate, 'X').format('DD/MM/YYYY')
-      const arrayPromise = lastRound.games.map((match) => {
-        const obj = {
-          userRef: request.userRef,
-          matchRef: match._id.toString()
-        }
-        return Promise.all([
-          getPredictionsRepository.getPredictions(obj, dictionary),
-          match
-        ])
-      })
-
-      const games = Promise.map(arrayPromise, (matchAndPrediction) => {
-        const prediction = matchAndPrediction[0]
-        const match = matchAndPrediction[1]
-        //TODO: verificar se alguns campos existir antes de inserir no obj
-
-        return {
-          matchRef: prediction.matchRef,
-          gamePontuation: prediction.matchPontuation,
-          homeTeamScore: match.homeTeamScore,
-          awayTeamScore: match.awayTeamScore,
-          homeTeamScoreGuess: prediction.guess.homeTeamScoreGuess,
-          awayTeamScoreGuess: prediction.guess.awayTeamScoreGuess,
-          homeTeam: match.homeTeam,
-          awayTeam: match.awayTeam
-        }
-      })
-
-      return Promise.all([games, guessLine, userPontuation, matchDayDate])
-    })
-    .then((obj) => {
-      const guessLine = obj[1]
-      const userPontuation = obj[2]
-      const matchDayDate = obj[3]
-
-      return {
-        championshipRef: guessLine.championship.championshipRef,
-        guessLinePontuation: userPontuation.totalPontuation,
-        matchDayPontuation: userPontuation.pontuationByMatchDay.find((matchDayPontuation) => matchDayPontuation.day === matchDayDate).pontuation,
-        games: obj[0]
-      }
-
-    })
+    .then((guessLine) => _getPontuationAndSomeNearMatchDay(guessLine, request, dictionary))
+    .then((pontuationAndMatchDayAndGuessLine) => _getPredictionPerMatchAndBuildMatchObj(pontuationAndMatchDayAndGuessLine, request, dictionary))
+    .then((promiseAllObj) => _buildResponseObj(promiseAllObj))
 
 }
 
-const _buildGamesArray = (predictions) => predictions.map((prediction) => {
+const _getPontuationAndSomeNearMatchDay = (guessLine, request, dictionary) => {
+  const repositoriesObj = {
+    championshipRef: guessLine.championship.championshipRef,
+    userRef: request.userRef
+  }
+  return Promise.all([
+    getPontuationsRepository(repositoriesObj, dictionary),
+    getLastRoundRepository(repositoriesObj, dictionary), 
+    guessLine])
+}
 
+const _getPredictionPerMatchAndBuildMatchObj = (pontuationAndMatchDayAndGuessLine, request, dictionary) => {
+  const userPontuation = pontuationAndMatchDayAndGuessLine[0]
+  const matchDay = pontuationAndMatchDayAndGuessLine[1]
+  const guessLine = pontuationAndMatchDayAndGuessLine[2]
+  const matchDayDate = moment(matchDay.unixDate, 'X').format('DD/MM/YYYY')
+
+  const predictionsPromiseArray = _buildPredictionsPromiseArray(matchDay, request.userRef, dictionary)
+
+  const games = _getMatchesArrayWithPredictionsAndResults(predictionsPromiseArray)
+
+  return Promise.all([games, guessLine, userPontuation, matchDayDate])
+}
+
+const _getMatchesArrayWithPredictionsAndResults = (predictionsPromiseArray) => 
+  Promise.map(predictionsPromiseArray, (matchAndPrediction) => {
+    const prediction = matchAndPrediction[0]
+    const match = matchAndPrediction[1]
+    //TODO: verificar se alguns campos existir antes de inserir no obj
+    //TODO: Fazer teste com prediction=undefined
+
+    return {
+      matchRef: prediction.matchRef,
+      gamePontuation: prediction.matchPontuation,
+      homeTeamScore: match.homeTeamScore,
+      awayTeamScore: match.awayTeamScore,
+      homeTeamScoreGuess: prediction.guess.homeTeamScoreGuess,
+      awayTeamScoreGuess: prediction.guess.awayTeamScoreGuess,
+      homeTeam: match.homeTeam,
+      awayTeam: match.awayTeam
+    }
   })
+
+const _buildPredictionsPromiseArray = (matchDay, userRef, dictionary) => 
+  matchDay.games.map((match) => {
+    const obj = {
+      userRef,
+      matchRef: match._id.toString()
+    }
+    return Promise.all([
+      getPredictionsRepository.getPredictions(obj, dictionary),
+      match
+    ])
+  })
+
+
+const _buildResponseObj = (promiseAllObj) => {
+  const games = promiseAllObj[0]
+  const guessLine = promiseAllObj[1]
+  const userPontuation = promiseAllObj[2]
+  const matchDayDate = promiseAllObj[3]
+
+  const responseObj = {
+    championshipRef: guessLine.championship.championshipRef,
+    guessLinePontuation: userPontuation.totalPontuation,
+    matchDayPontuation: _getMatchDayPontuation(matchDayDate, userPontuation),
+    games
+  }
+
+  return responseObj
+}
+
+const _getMatchDayPontuation = (matchDayDate, userPontuation) => {
+  //TODO: Fazer teste com pontuation not found
+  //TODO: Fazer teste com pontuation found
+  const MATCH_DAY_NOT_FOUND_PONTUATION = 0
+  const matchDayFound = userPontuation.pontuationByMatchDay.find((matchDayObj) => matchDayObj.day === matchDayDate)
+  if (matchDayFound) {
+    return matchDayFound.pontuation
+  }
+  return MATCH_DAY_NOT_FOUND_PONTUATION
+}
+
 module.exports = getGuessLine
 
 //TODO: Seguir passos:
@@ -92,3 +117,6 @@ module.exports = getGuessLine
 //getPrediction daquelas matchIDs
 
 //TODO: trocar nomenclatura de lastRound para nextMatchDay
+
+
+/*eslint no-magic-numbers: 0*/
