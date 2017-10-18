1aebe6bcca6fa3db5ee98e719e886e10edd199ce
Lucas Alves <lucashalves@live.com>
2017-10-18 19:23:08 -0200

    Refactoring subValidations

--- src/models/subValidations/championshipUserKey.js
+++ src/models/subValidations/championshipUserKey.js
@@ -1,16 +1,15 @@
 'use strict'
 
+const queryUtils = require('iguess-api-coincidents').Utils.queryUtils
+
 const CHAMPIONSHIP_POSITION = 0
 const USER_POSITION = 1
-
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Types.ObjectId
 
 const checkChampionshipUserKey = (key) => {
   const deconstructedKey = key.split('_')
 
-  return ObjectId.isValid(deconstructedKey[CHAMPIONSHIP_POSITION]) &&
-    ObjectId.isValid(deconstructedKey[USER_POSITION])
+  return queryUtils.isValidId(deconstructedKey[CHAMPIONSHIP_POSITION]) &&
+    queryUtils.isValidId(deconstructedKey[USER_POSITION])
 }
 
 module.exports = checkChampionshipUserKey
