1664ea6f20bfc7ed5ba13fc9fc0a208d38307c21
Lucas Alves <lucashalves@live.com>
2017-10-27 10:46:52 -0200

    FInishing setPrediction Route Schema response

--- src/routes/schemas/guessLine/setPredictions/setPredictionsSchemaResponse.js
+++ src/routes/schemas/guessLine/setPredictions/setPredictionsSchemaResponse.js
@@ -1,14 +1,19 @@
 'use strict'
 
 const Joi = require('joi')
+const coincidents = require('iguess-api-coincidents')
+
+const Config = coincidents.Config
+const ID_SIZE = Config.mongo.idStringSize
+const MIN_POSSIBLE_SCORE = Config.guess.minPossibleScore
 
 const setPredictionsSchemaResponse = Joi.object({
   predictions: Joi.array().items(Joi.object({
-    matchRef: Joi.string().required(),
+    matchRef: Joi.string().length(ID_SIZE).required(),
     prediction: Joi.object({
-      homeTeamScoreGuess: Joi.number().required(),
-      awayTeamScoreGuess: Joi.number().required()
-    })
+      homeTeamScoreGuess: Joi.number().min(MIN_POSSIBLE_SCORE).integer().required(),
+      awayTeamScoreGuess: Joi.number().min(MIN_POSSIBLE_SCORE).integer().required()
+    }).required()
   })).required(),
   alertMessage: Joi.string().allow('')
 })
