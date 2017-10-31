6f624716a91051afbea163f302bc8ef12a8e7717
Lucas Alves <lucashalves@live.com>
2017-10-31 19:55:14 -0200

    PutAdm integrated tests added

---
+++ test/integratedTests/guessLeague/putAdministrator/injectedRequests.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const happyPathRequest = {
+  method: 'PATCH',
+  url: '/guessleague/putAdministrator',
+  payload: {
+    'guessLeagueRef': '59c05e253feecf1e2898a3fb',
+    'userRef': '59b54e44a7631d433470fee7',
+    'userRefToAdm': '5932d84626fee5502cb422d6'
+  }
+}
+
+const userInvitedIsAlreadyAdm = {
+  method: 'PATCH',
+  url: '/guessleague/putAdministrator',
+  payload: {
+    'guessLeagueRef': '59c05e253feecf1e2898a3fb',
+    'userRef': '59b54e44a7631d433470fee7',
+    'userRefToAdm': '591df6c78d1fdc0bb4eba371'
+  }
+}
+
+const userRefEqualUserRefToAdm = {
+  method: 'PATCH',
+  url: '/guessleague/putAdministrator',
+  payload: {
+    'guessLeagueRef': '59c05e253feecf1e2898a3fb',
+    'userRef': '59b54e44a7631d433470fee7',
+    'userRefToAdm': '59b54e44a7631d433470fee7'
+  }
+}
+
+module.exports = {
+  happyPathRequest,
+  userInvitedIsAlreadyAdm,
+  userRefEqualUserRefToAdm
+}
---
+++ test/integratedTests/guessLeague/putAdministrator/putAdministrator.test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const Lab = require('lab')
+const Joi = require('joi')
+const coincidents = require('iguess-api-coincidents')
+
+const injectedRequests = require('./injectedRequests')
+const server = require('../../../../app')
+const schemaValidate = require('../../../../src/routes/schemas/guessLeague/administration/putAdministratorSchema').response
+const GuessLeague = require('../../../../src/models/guessDB/guessesLeaguesModel')
+
+const statusCode = coincidents.Utils.statusUtils
+const dictionary = coincidents.Translate.gate.selectLanguage()
+const lab = exports.lab = Lab.script()
+const expect = Lab.expect
+
+lab.experiment('Integrated Test ==> putAdministrator', () => {
+
+  lab.before((done) => {
+    const searchQuery = { _id: '59c05e253feecf1e2898a3fb' }
+    const updateQuery = { $pull: { administrators: '5932d84626fee5502cb422d6' } }
+    GuessLeague.update(searchQuery, updateQuery)
+      .then(() => done())
+  })
+
+  lab.test('putAdministrator HappyPath', (done) => {
+    server.inject(injectedRequests.happyPathRequest)
+      .then((response) => {
+        const result = response.result
+        expect(response.statusCode).to.be.equal(statusCode.ok)
+        Joi.validate(result, schemaValidate, (err) => {
+          expect(err).to.be.equal(null)
+          done()
+        })
+      })
+  })
+
+     lab.test('putAdministrator userRefIsNotAtGuessLeague', (done) => {
+      server.inject(injectedRequests.userInvitedIsAlreadyAdm)
+        .then((response) => {
+          const result = response.result
+          expect(result.message).to.be.equal(dictionary.anyGuessLeagueFound)
+          expect(response.statusCode).to.be.equal(statusCode.notFound)
+          done()
+        })
+    })
+
+    lab.test('putAdministratoruserRefEqualUserRefToAdm', (done) => {
+      server.inject(injectedRequests.userRefEqualUserRefToAdm)
+        .then((response) => {
+          const result = response.result
+          expect(result.message).to.be.equal(dictionary.youCantBeTheUserAndUserAdm)
+          expect(response.statusCode).to.be.equal(statusCode.conflict)
+          done()
+        })
+    })
+
+})
