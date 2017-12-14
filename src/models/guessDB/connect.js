5870f0a662183661b2f331c5824f42dd2a9e5dca
Lucas Alves <lucashalves@live.com>
2017-12-13 23:29:06 -0200

    Bug fix, mongodb connect and yaml changes

--- src/models/guessDB/connect.js
+++ src/models/guessDB/connect.js
@@ -3,7 +3,11 @@
 const coincidents = require('iguess-api-coincidents')
 
 const Managers = coincidents.Managers
+const mongo = coincidents.Config.mongo
+const GUESS_DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`
+let address = ''
 
-const db = Managers.mongoManager()
+mongo.guessAddress ? address = mongo.guessAddress : address = GUESS_DB_URI
+const db = Managers.mongoManager(address)
 
 module.exports = db
