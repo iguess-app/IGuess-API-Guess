162a84a0bb5ac66c49774cecb9ee15ec18f77260
Lucas Alves <lucashalves@live.com>
2018-05-20 15:21:18 -0300

    Added some tests about translate

--- test/integratedTests/guessLine/listLinesByLeague/listLinesByLeague.test.js
+++ test/integratedTests/guessLine/listLinesByLeague/listLinesByLeague.test.js
@@ -2,12 +2,14 @@
 
 const Joi = require('joi')
 const Lab = require('lab')
+const coincidents = require('iguess-api-coincidents')
 
 const stubs = require('../../../lib/stubs')
 const injectedRequests = require('./injectedRequests')
 const server = require('../../../../app')
 const schemaValidate = require('../../../../src/routes/schemas/guessLine/listLinesByLeague/listLinesByLeagueSchema').response
 
+const portugueseDictionary = coincidents.Translate.gate.selectLanguage('pt-br')
 const lab = exports.lab = Lab.script()
 const expect = Lab.expect
 
@@ -23,6 +25,20 @@ lab.experiment('Integrated Test ==> listLinesByLeague', () => {
     server.inject(injectedRequests.happyPathRequest)
       .then((response) => {
         const result = response.result
+        Joi.validate(result, schemaValidate, (err) => {
+          expect(err).to.be.equal(null)
+          done()
+        })
+      })
+  })
+
+  lab.test('listLinesByLeague Translated HappyPath', (done) => {
+    stubs.stubSessionRedis(injectedRequests.happyPathTranslatedRequest.headers.token)
+    server.inject(injectedRequests.happyPathTranslatedRequest)
+      .then((response) => {
+        const result = response.result
+        expect(result[0].championship).to.be.equal(portugueseDictionary.brazilian)
+        
         Joi.validate(result, schemaValidate, (err) => {
           expect(err).to.be.equal(null)
           done()
